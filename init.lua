c9499c91f74457640811fb4fed9e8d8ba526ac1a
Abdürrahim YILMAZ <ayilmaz@ayilmaz.xyz>
2024-09-28 14:55:01 +0300

    organize

--- init.lua
+++ init.lua
@@ -11,218 +11,173 @@ vim.opt.foldmethod = 'marker'
 
 vim.opt.shortmess:append("I")
 
+vim.opt.termguicolors = true
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- {{{ PACKAGES
 require('packer').startup(function()
 	use 'wbthomason/packer.nvim'  -- Package manager itself
 
-	-- THEMES
+	-- Themes
 	use 'catppuccin/nvim'
 	use 'tanvirtin/monokai.nvim'
 
-	-- PLUGINS
-	use {"ellisonleao/glow.nvim", config = function() require("glow").setup() end}
-	use 'nvim-treesitter/nvim-treesitter'
-	use {
-		'nvim-neo-tree/neo-tree.nvim',
-		branch = "v3.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-			"3rd/image.nvim"
-		}
-	}
-	use 'NvChad/nvim-colorizer.lua'
-	use {
-		'numToStr/Comment.nvim',
-		config = function()
-			require('Comment').setup()
-		end
-	}
+	-- Neo-Tree (File Explorer)
+	use 'nvim-neo-tree/neo-tree.nvim'
+	use 'nvim-lua/plenary.nvim'
+	use 'nvim-tree/nvim-web-devicons'
+	use 's1n7ax/nvim-window-picker'
+	use 'MunifTanjim/nui.nvim'
+	use '3rd/image.nvim'
+
+	-- LSP & CMP
+	use 'williamboman/mason.nvim'
 	use 'williamboman/mason-lspconfig.nvim'
+
 	use 'neovim/nvim-lspconfig'
+	use 'onsails/lspkind.nvim'
+
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/cmp-path'
 	use 'hrsh7th/cmp-cmdline'
-	
-	use 'nvim-telescope/telescope.nvim'
 
-	use "lukas-reineke/indent-blankline.nvim"
-	use 'hrsh7th/nvim-cmp'
-	use 'L3MON4D3/LuaSnip'
-	use 'onsails/lspkind.nvim'
-	use 'williamboman/mason.nvim'
-	use 'folke/trouble.nvim'
-	use 'hoob3rt/lualine.nvim'
+	use 'saadparwaiz1/cmp_luasnip'
+	use 'hrsh7th/nvim-cmp' -- Completions
+	use 'L3MON4D3/LuaSnip' -- Snippet Engine
 
-	use 'mrjones2014/smart-splits.nvim'
-	use 'mrjones2014/legendary.nvim'
-	use "numToStr/FTerm.nvim"
-
-	use {
-		'goolord/alpha-nvim',
-		requires = { 'echasnovski/mini.icons' },
-		config = function ()
-			require'alpha'.setup(require'alpha.themes.dashboard'.config)
-		end
-	}
+	-- OTHER
+	use 'nvim-treesitter/nvim-treesitter' -- Highlighter
+	use 'NvChad/nvim-colorizer.lua' -- Colorizer
+	use 'numToStr/Comment.nvim' -- Auto-comment
+	use 'nvim-telescope/telescope.nvim' -- Fuzzy finder
+	use 'lukas-reineke/indent-blankline.nvim' -- Indent Line
+	use 'hoob3rt/lualine.nvim' -- Status Bar
+	use 'mrjones2014/smart-splits.nvim' -- Split Manager
+	use 'romgrk/barbar.nvim' -- Tabs Manager
+	use 'mrjones2014/legendary.nvim' -- Keymap Manager
+	use 'numToStr/FTerm.nvim' -- Floating Terminal
+	use 'lewis6991/gitsigns.nvim' -- Git Signs
+	use 'ellisonleao/glow.nvim' -- Markdown previewer
 end)
 -- }}}
 
--- {{{ NVIM TREESITTER
+-- {{{ TREESITTER
 require('nvim-treesitter.configs').setup {
- ensure_installed = "all",
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+	ensure_installed = 'all',
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 }
 -- }}}
 
 -- {{{ NEOTREE 
 require('neo-tree').setup({
-	popup_border_style = "rounded",
+	close_if_last_window = true,
+	popup_border_style = 'rounded',
     enable_git_status = true,
     enable_diagnostics = true,
 	filesystem = {
 		filtered_items = {
-			visible = true,
-			hide_dotfiles = false,
+			visible = false,
+			hide_dotfiles = true,
 			hide_gitignored = false,
-			hide_hidden = false
+			hide_hidden = true
 		},
-		hijack_netrw_behavior = "open_default"
-	}
+		hijack_netrw_behavior = 'open_default'
+	},
+	window = {
+		position = 'right',
+	},
 })
 -- }}}
 
 -- {{{ LUALINE
-require('lualine').setup { -- status line
-  options = {
-    icons_enabled = true,
-    component_separators = { left = '|', right = '|' },
-    section_separators = { left = '', right = '' },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', 'lsp_progress'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
+require('lualine').setup {
+	options = {
+		icons_enabled = true,
+		-- component_separators = { left = '|', right = '|' },
+		-- section_separators = { left = '', right = '' },
+		component_separators = { left = '', right = ''},
+	    section_separators = { left = '', right = ''},
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = {'mode'},
+		lualine_b = {'branch', 'diff', 'diagnostics'},
+		lualine_c = {'filename', 'lsp_progress'},
+		lualine_x = {'encoding', 'filetype'},
+		lualine_y = {'progress'},
+		lualine_z = {'location'}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	extensions = {},
+	options = { disabled_filetypes = {'neo-tree'} }
 }
 -- }}}
 
 -- {{{ CMP
 local cmp = require('cmp')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+local lspconfig = require('lspconfig')
+
 cmp.setup({
-  snippet = {
-  expand = function(args)
-    require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  window = {},
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-	['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	window = {},
+	mapping = cmp.mapping.preset.insert({
+		['<C-Up>'] = cmp.mapping.scroll_docs(-4),
+		['<C-Down>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({ { name = 'nvim_lsp' }, { name = 'luasnip' } }, { { name = 'buffer' } }),
+	formatting = {
+		format = require('lspkind').cmp_format({
+			mode = 'symbol',
+			maxwidth = 50,
+			ellipsis_char = '...',
+			show_labelDetails = true,
+			before = function (entry, vim_item)
+				return vim_item
+			end
+		})
+	}
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+	mapping = cmp.mapping.preset.cmdline(),
+    sources = { { name = 'buffer' } }
 })
 
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
+    matching = { disallow_symbol_nonprefix_matching = false }
 })
 
-local lspkind = require('lspkind')
-cmp.setup {
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol',
-      maxwidth = 50,
-      ellipsis_char = '...',
-      show_labelDetails = true,
-      before = function (entry, vim_item)
-        return vim_item
-      end
-    })
-  }
-}
-
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
-
 mason.setup()
-mason_lspconfig.setup();
-mason_lspconfig.setup_handlers {
-  function (server_name)
-    lspconfig[server_name].setup {}
-  end,
-  ["volar"] = function()
-    lspconfig.volar.setup({
-      filetypes = { "vue" },
-      init_options = {
-        vue = {
-          hybridMode = false,
-        },
-        typescript = {
-          tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
-        },
-      },
-    })
-  end,
-}
--- }}}
-
--- {{{ rust_analyzer
-local util = require('lspconfig/util')
-lspconfig.rust_analyzer.setup({
-  filetypes = {"rust"},
-  root_dir = util.root_pattern("Cargo.toml"),
-  settings = {
-    ['rust-analyzer'] = {
-      cargo = {
-        allFeatures = true;
-      },
-    },
-  },
+mason_lspconfig.setup()
+mason_lspconfig.setup_handlers({
+	function (server_name)
+		lspconfig[server_name].setup({})
+	end,
 })
 -- }}}
 
@@ -241,11 +196,29 @@ vim.keymap.set('t', '<F3>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 require('legendary').setup({
 	keymaps = {
 		{ '<F2>', ':Neotree toggle<CR>' },
-		{ '<F3>', '<CMD>lua require("FTerm").toggle()<CR>' },
-		{ 'ff', '<cmd>Telescope find_files<cr>' },
-		{ 'fg', '<cmd>Telescope live_grep<cr>' },
-		{ 'fb', '<cmd>Telescope buffers<cr>' },
-		{ 'fh', '<cmd>Telescope help_tags<cr>' }
+		{ '<F3>', ':lua require("FTerm").toggle()<CR>' },
+		{ 'ff', ':Telescope find_files<CR>' },
+		{ 'fg', ':Telescope live_grep<CR>' },
+		{ 'fb', ':Telescope buffers<CR>' },
+		{ 'fh', ':Telescope help_tags<CR>' },
+		{ 'gt', ':BufferNext<CR>' },
+		{ 'gT', ':BufferPrevious<CR>' },
+		{ '<A-1>', ':BufferGoto 1<CR>' },
+		{ '<A-2>', ':BufferGoto 2<CR>' },
+		{ '<A-3>', ':BufferGoto 3<CR>' },
+		{ '<A-4>', ':BufferGoto 4<CR>' },
+		{ '<A-5>', ':BufferGoto 5<CR>' },
+		{ '<A-6>', ':BufferGoto 6<CR>' },
+		{ '<A-7>', ':BufferGoto 7<CR>' },
+		{ '<A-8>', ':BufferGoto 8<CR>' },
+		{ '<A-9>', ':BufferGoto 9<CR>' },
+		{ '<A-0>', ':BufferLast<CR>' },
+		{ '<A-c>', ':BufferClose<CR>' },
+		{ '<A-p>', ':BufferPin<CR>' },
+		{ '<A-Left>', ':BufferPrevious<CR>' },
+		{ '<A-Right>', ':BufferNext<CR>' },
+		{ '<A-Up>', ':BufferPick<CR>' },
+		{ '<A-Down>', ':BufferPick<CR>' },
 	},
 	commands = {},
 	funcs = {},
@@ -260,11 +233,24 @@ require('legendary').setup({
 		},
 	},
 })
-
 -- }}}
 
-require("ibl").setup() -- indent blank lines
-require("telescope").setup()
-require("colorizer").setup()
+-- {{{ BARBAR
+require('barbar').setup({
+	animation = false,
+	clickable = false,
+	icons = {
+		button = '',
+		pinned = {button = '', filename = true},
+	},
+	tabpages = false,
+})
+-- }}}
+
+require('Comment').setup()
+require('ibl').setup()
+require('telescope').setup()
+require('colorizer').setup()
+require('glow').setup()
 
 vim.cmd("colorscheme catppuccin-macchiato")
